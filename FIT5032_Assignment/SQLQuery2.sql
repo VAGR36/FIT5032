362323df9d381d7a3227643425420ca3f5610167
Daiwei <dyan0041@student.monash.edu>
2023-10-19 15:56:25 +1100

    Add empty value check to appointments

--- FIT5032_Assignment/SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE CTMachine(
-    ID INT PRIMARY KEY IDENTITY(1,1),
-    Name NVARCHAR(50) NOT NULL,
-    Description NVARCHAR(50) NOT NULL,
-    Status NVARCHAR(50) NOT NULL,
-    )
-
-    INSERT INTO CTMachine VALUES('Machine_1','This is the first CT scan machine', 'Available')
-    INSERT INTO CTMachine VALUES('Machine_2','This is the second CT scan machine', 'Unavailable')
